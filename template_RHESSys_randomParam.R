f84e05e0951d3584c0b3d9fe78faa5f0acaf14a4
Laurence Lin <laurence.lin@icloud.com>
2019-03-07 11:31:37 -0500

    Update template_RHESSys_randomParam.R

--- template_RHESSys_randomParam.R
+++ template_RHESSys_randomParam.R
@@ -1,76 +1,77 @@
 
 
-########## parameter settings
+RHESSysParamBoundaryDefault = data.frame(s1=c(0.001,20))
+RHESSysParamBoundaryDefault $s2 = c(0.1,300.0)
+RHESSysParamBoundaryDefault $s3 = c(0.1,20)
+RHESSysParamBoundaryDefault $sv1 = c(0.001,20)
+RHESSysParamBoundaryDefault $sv2 = c(0.1,300.0)
+RHESSysParamBoundaryDefault $gw1 = c(0.001,0.2)
+RHESSysParamBoundaryDefault $gw2 = c(0.001,0.2)
+RHESSysParamBoundaryDefault $snowEs = c(0.5,2)
+RHESSysParamBoundaryDefault $snowTs = c(0.5,2)
 
-	paramBoundary = matrix(NA,8,2)
-	rownames(paramBoundary)=c('s1','s2','s3','sv1','sv2','gw1','gw2', 'snowenergy')
-	colnames(paramBoundary)=c('min','max')
-	paramBoundary[1,]=c(0.001,20) #s1
-	paramBoundary[2,]=c(0.1,300.0) #s2
-	paramBoundary[3,]=c(0.1,1) #s3 
-	paramBoundary[4,]=c(0.001,20) #sv1
-	paramBoundary[5,]=c(0.1,300.0) #sv2
-	paramBoundary[6,]=c(0.001,0.2) #gw1 
-	paramBoundary[7,]=c(0.001,0.2) #gw2 
-	paramBoundary[8,]=c(0.5,2) #snowenergy
+# for example,
+# RHESSys_arg = paste(
+		# '-st 1995 1 1 1 -ed 2011 10 1 1',
+		# '-b -t tecfiles/tec_daily.txt',
+		# '-w worldfiles/combine_oldwayRZ5_soiltest3b',
+		# '-whdr worldfiles_ws18/ws18Bolstadcomb.hdr',
+		# '-r flow_ws18/ws18Bolstad.flow')
+
+RivannaJobs=function(RHESSys_arg, outputFOLDER, param, outputfile){
 	
-
-########## job settings
-
-	outputfile = 'parallelRun103.sh'
-	jobIDnum = 12323
-	outputFOLDER = 'output'
 	
-	itr = 1:500
-	param = cbind(
-		itr,
-		runif(length(itr), paramBoundary[1,1], paramBoundary[1,2]),
-		runif(length(itr), paramBoundary[2,1], paramBoundary[2,2]),
-		runif(length(itr), paramBoundary[3,1], paramBoundary[3,2]),
-		runif(length(itr), paramBoundary[4,1], paramBoundary[4,2]),
-		runif(length(itr), paramBoundary[5,1], paramBoundary[5,2]),
-		runif(length(itr), paramBoundary[6,1], paramBoundary[6,2]),
-		runif(length(itr), paramBoundary[7,1], paramBoundary[7,2]),
-		runif(length(itr), paramBoundary[8,1], paramBoundary[8,2])
-	)	
+	jobIDnum = as.numeric(Sys.time())
 	
-########## platform settings
-
 	sbatch_cluster_header = paste('sbatch -o ', outputFOLDER, '/log.txt -J p', jobIDnum ' --export=v=\'', sep='')
 	sbatch_cluster_end = '\' Rivanna_std.sh'
-	
-	bsub_cluster_header = paste('bsub -q day -G xxx_pi -M 1 -o ', outputFOLDER, '/log.txt ./rhesys_bin ', sep='') 
-	bsub_cluster_end = ''
-	
-	local_header = './rhessys_bin '
-	local_end = ''
-		
-########## writing the script
-
-	## choose platform
 	cmd_header = sbatch_cluster_header
 	cmd_end = sbatch_cluster_end
 	
-write('#!/bin/bash', outputfile, append=F)
-for(i in 1:length(itr)){
-	 
-	RHESSys_arg = paste(
-		'-st 1995 1 1 1 -ed 2011 10 1 1',
-		'-b -t tecfiles/tec_daily.txt',
-		'-w worldfiles/combine_oldwayRZ5_soiltest3b',
-		'-whdr worldfiles_ws18/ws18Bolstadcomb.hdr',
-		'-r flow_ws18/ws18Bolstad.flow',
-		'-pre', paste(outputFOLDER,'/rhessys',param[i,1],sep=''),
-		paste("-s",param[i,2],param[i,3],param[i,4]),
-		paste("-sv",param[i,5],param[i,6]),
-		paste("-gw",param[i,7],param[i,8])
-		#paste('-snowenergy',param[i,9]), # not calibrating this parameter
-	)
 	
-	cmd = paste(cmd_header, RHESSys_arg, cmd_end, sep='')
+	paramNames=colnames(param)
+	if('s3'%in% paramNames){
+		sParamLine = paste('-s',paste(param[,'s1'],param[,'s2'],param[,'s3']))
+	}else{
+		sParamLine = paste('-s',paste(param[,'s1'],param[,'s2']))
+	}
+	svParamLine = paste('-sv',paste(param[,'sv1'],param[,'sv2']))
+	gwParamLine = paste('-gw',paste(param[,'gw1'],param[,'gw2']))
+	otherParamLine = ''
 	
-	write(cmd, outputfile,append=T)
-}
+	# check for non default parameter	
+	cond = !(paramNames %in% c("itr","s1","s2","sv1","sv2","gw1","gw2"))
+	if(sum(cond)>0){
+		otherParam = paramNames[cond]
+		otherParamLine = paste(paste('-',otherParam[j],sep=''),param[, otherParam[j]])
+		for(j in 2:length(otherParam)) otherParamLine = paste(otherParamLine, paste(paste('-',otherParam[j],sep=''),param[, otherParam[j]]))
+				
+		AllparamLine = paste(sParamLine, svParamLine, gwParamLine, otherParamLine)
+	}else{
+		AllparamLine = paste(sParamLine, svParamLine, gwParamLine)
+	}
+
+	write('#!/bin/bash', outputfile, append=F)
+	for(i in 1:dim(param)[1]){
+
+		cmd = paste(
+			cmd_header, 
+			paste(
+				RHESSys_arg, 
+				'-pre', paste(outputFOLDER,'/rhessys',param[i,1],sep=''),
+				AllparamLine
+			), 
+			cmd_end, sep='')
+		
+		write(cmd, outputfile,append=T)
+	}#i
+	
+}#function
+
+
+
+
+
 
 
 
