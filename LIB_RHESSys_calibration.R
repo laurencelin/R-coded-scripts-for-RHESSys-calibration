2eeefd83317bbced301798eeee8e77e32c064e15
Laurence Lin <laurence.lin@icloud.com>
2020-04-07 11:01:53 -0400

    update model evaluation

--- LIB_RHESSys_calibration.R
+++ LIB_RHESSys_calibration.R
@@ -171,6 +171,37 @@ evaluateModelQuick = function(passedArgList, passedArgParamBoundary, topPrecent=
         ##......... function calling
         w = modelFittness( as.numeric(calobs[calobs.dtsm,'mmd']), rhessys_SingleFile[rhessys.dtsm,], DTStable);
         print(round(w$FittnessList,2))
+        
+        ##---------------------------------------- Diagnosis
+		dev.new(width=14, height=8)
+		layout(matrix(1:4,nrow=4)); 
+		par(mar=c(0,3,1,3))
+		plot(plotTime, rhessys_SingleFile[rhessys.dtsm,'precip'], col='lightblue', lwd=2,type='l', ylab='',xaxt='n',bty='n', ylim=rev(range(rhessys_SingleFile[rhessys.dtsm,'precip'])))
+		abline(h=0)
+		par(new = TRUE)
+		plot(plotTime, rhessys_SingleFile[rhessys.dtsm,'tmin'], col=t_col('lightblue1'), bty='l',lty=2, type='l', ylim=c(-20,40), yaxt='n', bty='n',xaxt='n')
+		lines(plotTime, rhessys_SingleFile[rhessys.dtsm,'tmax'], col= t_col('lightpink'), lty=2)
+		lines(plotTime, rhessys_SingleFile[rhessys.dtsm,'tavg'], col= t_col('gray'))
+		abline(h=0, lty=2, col='gray')
+		axis(4)
+		par(mar=c(0,3,0,3))
+		upper = max(rhessys_SingleFile[rhessys.dtsm,'streamflow'],calobs[calobs.dtsm,'mmd'])
+		lower = min(rhessys_SingleFile[rhessys.dtsm,'streamflow'],calobs[calobs.dtsm,'mmd'])
+		plot(plotTime, rhessys_SingleFile[rhessys.dtsm,'streamflow'], col='blue', lwd=2,type='l', ylab='', ylim=c(lower ,upper),bty='n',xaxt='n')
+		lines(plotTime, calobs[calobs.dtsm,'mmd'], col='red')
+		lines(plotTime, rhessys_SingleFile[rhessys.dtsm,'pet'], col='gray')
+		lines(plotTime, rhessys_SingleFile[rhessys.dtsm,'evap']+rhessys_SingleFile[rhessys.dtsm,'trans'], col='green')
+		lines(plotTime, rhessys_SingleFile[rhessys.dtsm,'X.sat_area'], lty=2,col=gray(0.8))
+		lines(plotTime, rhessys_SingleFile[rhessys.dtsm,'snowpack'],col='lightblue')
+		par(mar=c(0,3,0,3))
+		plot(plotTime, rhessys_SingleFile[rhessys.dtsm,'sat_def_z'],type='l', ylab='',xaxt='n',bty='n', ylim=rev(range(rhessys_SingleFile[rhessys.dtsm,'sat_def_z'])))
+		par(mar=c(3,3,0,3))
+		upper = max(rhessys_SingleFile[rhessys.dtsm,'streamflow'],calobs[calobs.dtsm,'mmd'])
+		lower = min(rhessys_SingleFile[rhessys.dtsm,'streamflow'],calobs[calobs.dtsm,'mmd'])
+		plot(plotTime, calobs[calobs.dtsm,'mmd'], col='red', type='l',log='y',bty='l', ylim=c(lower ,upper))
+		lines(plotTime, rhessys_SingleFile[rhessys.dtsm,'streamflow'], col='blue' )
+		lines(plotTime, rhessys_SingleFile[rhessys.dtsm,'baseflow'], col='darkblue',lty=2 )
+        
     }, error = function(e){
         print(paste('file', passedArgList$RHESSysOutput, ' is incorrect/missing/corrupted.'))
     })#try blocks
